aa712df983a13831b0fd39a334a74ceb326f8c5d
Talha Code Lab <127521717+talhacodelab97@users.noreply.github.com>
2024-02-29 08:15:27 +0500

    portfolio created as task

---
+++ main.js
@@ -0,0 +1,8 @@
+// Setup and Animation
+  var typed = new Typed('.text', {
+    strings: ['Frontend Developer', 'Youtuber', 'Web Developer'],
+    typeSpeed: 100,
+    backspeed:100,
+    backDelay: 1000,
+    loop:true
+  });
